09e748bbd2cebd29d43251076b10a8a3f18c4d7c
youba idjis <youba.etud@gmail.com>
2023-10-20 21:07:12 -0700

    done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,166 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Our workout Program with photos</h1>
+        <a href='https://www.docteur-fitness.com/exercice-musculation'>Website source</a>
+        <ol>
+          <li>
+            <h2>Pectoraux, Epaules LT AV, Triceps</h2>
+            <ul>
+              <li><h4>Pectoraux</h4></li>
+              <ol>
+
+                <li>
+                  <p>Dev coucher 4 x 8-12</p>
+                  <img alt='dev coucher' src="https://www.docteur-fitness.com/wp-content/uploads/2019/08/developpe-couche.gif" />
+                </li>
+                <li>
+                  <p>Ecarte coucher 4 x 8-12</p>
+                  <img alt='ecarter coucher' src='https://www.docteur-fitness.com/wp-content/uploads/2022/09/ecarte-couche-haltere.gif' />
+                </li>
+                <li>
+                  <p>Dev incline 3 x 8 /12</p>
+                  <img alt='dev incline' src='https://www.docteur-fitness.com/wp-content/uploads/2021/10/developpe-incline-barre.gif' />
+                </li>
+                <li>
+                  <p>Ecarter incline 3 x 8 /12 </p>
+                  <img alt='ecarter incline' src='https://www.docteur-fitness.com/wp-content/uploads/2022/09/ecartes-incline-avec-halteres.gif' />
+                </li>
+                <li>
+                  <p>Dips 3 x max</p>
+                  <img alt='dips' src='https://www.docteur-fitness.com/wp-content/uploads/2000/01/dips-pectoraux.gif' />
+                </li>
+              </ol>
+
+              <li><h4>Epaules LT/AV</h4></li>
+              <ol>
+                <li>
+                  <p>Developpe assis presse 4 x 12</p>
+                  <img alt='dev assis presse' src='https://www.docteur-fitness.com/wp-content/uploads/2022/02/developpe-epaule-halteres.gif' /></li>
+                <li>
+                  <p>Elevation laterale avec haltere 3 x 12</p>
+                  <img alt='laterale avec altere' src='https://www.docteur-fitness.com/wp-content/uploads/2000/08/elevations-laterales-exercice-musculation.gif' />
+                </li>
+                <li>
+                  <p>élévation frontale avec haltères 3 x 12</p>
+                  <img alt='élévation frontale avec haltères' src='https://www.docteur-fitness.com/wp-content/uploads/2000/08/elevations-frontales-exercice-musculation.gif' />
+                </li>
+              </ol>
+
+              <li><h4>Triceps</h4></li>
+              <ol>
+                <li>
+                  <p>Barre front prise supination 3 x 12</p>
+                  <img alt='barre front prise supination' src='https://www.fitadium.com/fstrz/r/s/www.fitadium.com/conseils/wp-content/uploads/2020/07/00351301-Barbell-Decline-Close-Grip-To-Skull-Press_Upper-Arms_720.gif?frz-v=1529' />
+                </li>
+                <li>
+                  <p>Barre front prise pronation 3 x 12  (same as the one above twist hand )</p>
+                  <img alt='Barre front prise pronation' src='https://www.fitadium.com/fstrz/r/s/www.fitadium.com/conseils/wp-content/uploads/2020/07/00351301-Barbell-Decline-Close-Grip-To-Skull-Press_Upper-Arms_720.gif?frz-v=1529' />
+                </li>
+                <li>
+                  <p>Kick back 4 x 12</p>
+                  <img alt='Kick back' src='https://www.docteur-fitness.com/wp-content/uploads/2021/09/kickback.gif' />
+                </li>
+              </ol>
+
+              <li><h4>Abdos</h4></li>
+              <ol>
+                <li>
+                  <p>relevé des jambes 4 x 15</p>
+                  <img alt='relevé des jambes' src='https://www.docteur-fitness.com/wp-content/uploads/2022/04/releve-jambes-chaise-romaine-abdominaux.gif' />
+                </li>
+              </ol>
+            </ul>
+          </li>
+
+          <li>
+            <h2>Dos, Epaules AR, Biceps</h2>
+            <ul>
+              <li><h4>Dos</h4></li>
+              <ol>
+                <li>
+                  <p>Lambert 4 x 12</p>
+                  <img alt='Lambert' src='https://julienquaglierini.com/wp-content/uploads/2017/10/extension-lombaire-au-banc.jpg.webp' />
+                </li>
+                <li>
+                  <p>Tractions 3 x max</p>
+                  <img alt='Tractions' src='https://www.docteur-fitness.com/wp-content/uploads/2022/02/traction-musculation-dos.gif' /></li>
+                <li>
+                  <p>Tirage verticale avant large 3 x 8 /12</p>
+                  <img alt='Tirage verticale avant large' src='https://www.docteur-fitness.com/wp-content/uploads/2022/01/tirage-vertical-prise-inversee.gif' /></li>
+                <li>
+                  <p>Rowing barre 3 x 8 /12</p>
+                  <img alt='rowing barre ' src='https://www.docteur-fitness.com/wp-content/uploads/2021/09/rowing-barre.gif' /></li>
+                <li>
+                  <p>Tirage horizontal 3 x 8 /12</p>
+                  <img alt='Tirage horizontal' src='https://www.docteur-fitness.com/wp-content/uploads/2022/02/tirage-horizontal-poulie.gif' /></li>
+              </ol>
+
+              <li><h4>Epaules AR</h4></li>
+              <ol>
+                <li>
+                  <p>Oiseau 3 x 12</p>
+                  <img alt='Oiseau' src='https://www.docteur-fitness.com/wp-content/uploads/2021/12/oiseau-assis-sur-banc.gif' />
+                </li>
+                <li>
+                  <p>Face pull 3 x 12</p>
+                  <img alt='' src='https://www.docteur-fitness.com/wp-content/uploads/2022/01/face-pull.gif' />
+                </li>
+              </ol>
+
+              <li><h4>Biceps</h4></li>
+              <ol>
+                <li>
+                  <p>Curl barre Exterieur 3 x 12</p>
+                  <img alt='' src='https://www.docteur-fitness.com/wp-content/uploads/2021/09/curl-barre.gif' />
+                </li>
+                <li>
+                  <p>Curl barre interieur 3 x 12</p>
+                  <img alt='' src='https://www.docteur-fitness.com/wp-content/uploads/2022/04/curl-inverse-barre.gif' />
+                </li>
+                <li>
+                  <p>Curl haltere alterne 3 x 12</p>
+                  <img alt='Curl haltere' src='https://www.docteur-fitness.com/wp-content/uploads/2022/08/curl-biceps-avec-halteres-alterne.gif' />
+                </li>
+                <li>
+                  <p>Curl marteau 3 x 12</p>
+                  <img alt='' src='https://www.docteur-fitness.com/wp-content/uploads/2021/10/curl-haltere-prise-marteau-pupitre.gif' />
+                </li>
+              </ol>
+
+              <li><h4>Abdos</h4></li>
+              <ol>
+                <li>
+                  <p>Crunch 4 x 15</p>
+                  <img alt='' src='https://www.docteur-fitness.com/wp-content/uploads/2000/07/crunch-au-sol-exercice-musculation.gif' />
+                </li>
+              </ol>
+            </ul>
+          </li>
+
+          <li>
+            <h2>Jambes</h2>
+            <ol>
+              <li>
+                <p>Squats 5 x 25/20/18/15/15</p>
+                <img style={{backgroundColor: "white"}} alt='Squat' src='https://enarahealth.com/wp-content/uploads/2022/03/PngItem_2810409-e1647565708516.png' />
+              </li>
+              <li>
+                <p>Leg curl 4 x 15</p>
+                <img alt='Leg curl' src='https://www.docteur-fitness.com/wp-content/uploads/2021/10/leg-curl-allonge.gif' />
+              </li>
+              <li>
+                <p>Leg extension 4 x 15</p>
+                <img alt='Leg extension' src='https://www.docteur-fitness.com/wp-content/uploads/2000/06/leg-extension-exercice-musculation.gif' />
+              </li>
+            </ol>
+          </li>
+
+
+        </ol>
       </header>
     </div>
   );
